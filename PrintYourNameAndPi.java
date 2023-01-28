8796e1c39d3a0056d1c2febd3113f3a10093c3fc
Cristian Valenti <122291593+CyberCribbio@users.noreply.github.com>
2023-01-28 19:57:52 +0100

    Add files via upload

---
+++ PrintYourNameAndPi.java
@@ -0,0 +1,10 @@
+public class PrintYourNameAndPi {
+    public static void main(String[] args) {
+        String name = "CRISTIAN";
+        double number = 3.141590;
+        System.out.printf("%S%n", "CRISTIAN");
+        System.out.printf("Il Pigreco è: %f" , 3.141590);
+
+    }
+}
+
